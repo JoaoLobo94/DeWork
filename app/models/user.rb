dcb6502495e6ebad26f2394fe7e03a032281e015
joao <joao.lobom@gmail.com>
2022-02-11 15:57:07 +0000

    Uninstall devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,8 +21,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
   has_many :user_companies
   has_many :user_contributions
   has_many :companies, through: :user_companies
